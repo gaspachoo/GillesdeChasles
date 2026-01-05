2a0e9cd73c6b05dae96522c0ea3854952d1e5595
Gaspachoo <gaspard.lefort@outlook.fr>
2026-01-05 19:34:18 +0100

    [FRONT] add routes for new pages

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,9 +1,30 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Poemes } from './pages/poemes/poemes';
+import { Reflexions } from './pages/reflexions/reflexions';
+import { Ailleurs } from './pages/ailleurs/ailleurs';
+import { Vous } from './pages/vous/vous';
 
 export const routes: Routes = [
     {
     path: '',
     component: Home,
   },
+  {
+    path: 'poemes',
+    component: Poemes,
+  },
+  {
+    path: 'reflexions',
+    component: Reflexions,
+  },
+  {
+    path: 'ailleurs',
+    component: Ailleurs,
+  },
+  {
+    path: 'vous',
+    component: Vous,
+  },
+  
 ];
