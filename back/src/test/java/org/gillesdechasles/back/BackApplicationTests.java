d209af7437d4844df6b924cd3dedfd21d1af7d2c
Gaspard L. <gaspard.lefort@etik.com>
2026-07-20 23:43:55 +0200

    [BACK] fix tests not working

--- back/src/test/java/org/gillesdechasles/back/BackApplicationTests.java
+++ back/src/test/java/org/gillesdechasles/back/BackApplicationTests.java
@@ -2,14 +2,11 @@ package org.gillesdechasles.back;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class BackApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
